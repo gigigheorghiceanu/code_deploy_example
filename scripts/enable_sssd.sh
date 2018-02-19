dac3ad64b7f977d25ac25912230af0bb3e0277dd
gigi.gheorghiceanu@gmail.com <gigi.gheorghiceanu@gmail.com>
2018-02-19 22:50:00 +0200

    test123

--- scripts/enable_sssd.sh
+++ scripts/enable_sssd.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+/bin/cp /opt/files/scripts/sssd.conf /etc/sssd/
+
 users=`cat /etc/passwd | cut -d ":" -f 1  | tr "\n" ","`
 sed -i "s/^filter_users.*/filter_users = ${users%?} /g" /etc/sssd/sssd.conf
 chmod 0000600 /etc/sssd/sssd.conf
--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-/bin/cp /opt/files/scripts/sssd.conf /etc/sssd/
+#/bin/cp /opt/files/scripts/sssd.conf /etc/sssd/
 
+echo " " >> /etc/ssh/sshd_config
 echo "AuthorizedKeysCommand /usr/bin/sss_ssh_authorizedkeys" >> /etc/ssh/sshd_config
 echo "AuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config
 
