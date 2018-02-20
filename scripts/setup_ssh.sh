cbfb04a279f0361bcc19f3871b5968c7f6ce58c8
gigi.gheorghiceanu@gmail.com <gigi.gheorghiceanu@gmail.com>
2018-02-20 11:48:23 +0200

    test123

--- scripts/setup_ssh.sh
+++ scripts/setup_ssh.sh
@@ -1,13 +1,22 @@
 #!/bin/bash
 
-
-echo " " >> /etc/ssh/sshd_config
-echo "AuthorizedKeysCommand /usr/bin/sss_ssh_authorizedkeys" >> /etc/ssh/sshd_config
-echo "AuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config
+# modify ssh config file: /etc/ssh/sshd_config
+a=$(cat /etc/ssh/sshd_config | grep "^AuthorizedKeysCommand /usr/bin/sss_ssh_authorizedkeys" | wc -l)
+b=$(cat /etc/ssh/sshd_config | grep "^AuthorizedKeysCommandUser nobody" | wc -l)
+if [ "$a" -eq "0" ] && [ "$b" -eq "0" ];then
+	echo " " >> /etc/ssh/sshd_config
+	echo "AuthorizedKeysCommand /usr/bin/sss_ssh_authorizedkeys" >> /etc/ssh/sshd_config
+	echo "AuthorizedKeysCommandUser nobody" >> /etc/ssh/sshd_config
+fi
 
 # add devops group in sudoers file
-echo "%devops ALL=(ALL)  ALL"  >> /etc/sudoers
+# check if group is already added:
+c=$(cat /etc/sudoers | grep %devops | wc -l)
+if [ "$c" -eq "0" ];then
+	echo "%devops ALL=(ALL)  ALL"  >> /etc/sudoers
+fi
 
 service sshd restart
 service sssd restart
+
 
